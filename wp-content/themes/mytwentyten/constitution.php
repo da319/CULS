00eced824b8afdf59b801397f0c2b4a4d7635733
Daiva Naudziuniene <da319@cam.ac.uk>
2010-11-06 00:27:59 +0200

    added english constitution

---
+++ wp-content/themes/mytwentyten/constitution.php
@@ -0,0 +1,105 @@
+<?php
+/*
+Template Name: Constitution
+*/
+
+get_header(); ?>
+
+		<div id="container">
+			<div id="content" role="main">
+				<div id="post" <?php post_class(); ?>>
+					<h1 class="entry-title"><?php _e('CONSTITUTION') ?></h1>
+					<h3>1. Name</h3>
+					<p>The name of the Society shall be "Cambridge University Lithuanian Society" hereafter referred to as "the Society".</p>
+					
+					<h3>2. Objects</h3>
+					<p>2.1 The Society's aims shall be to promote and maintain</p>
+					<ul>
+						<li>Lithuanian culture and heritage among Lithuanian Cambridge scholars and friends.</li>
+					</ul>
+					<p>2.2 In pursuance of its aims the Society will</p>
+					<ul>
+						<li>bring together Lithuanian students reading in University of Cambridge,</li>
+						<li>celebrate national commemorations and feasts,</li>
+						<li>spread Lithuanian culture and heritage by organizing social events,</li>
+						<li>help both existing or future students to be integrated into the Cambridge University life,</li>
+						<li>found an alumni network for the future cooperation and communication,</li>
+						<li>establish and sustain links with other Cambridge University societies and serve for the benefit of friendship and happiness.</li>
+					</ul>
+					
+					<h3>3. MEMBERSHIP</h3>
+					<p>3.1 Membership of the Society shall be open to all members of the University and any others that 
+					have an interest in the Lithuanian culture, heritage and language.</p>
+					<p>3.2 There shall be an annual fee for membership which shall be determined by the 
+					Society from time to time.</p>
+					<p>3.3 The Society may offer Life Membership and Honorary Membership on such terms as it may decide.</p>
+					<p>3.4 The Executive Committee may expel any member whose conduct seems likely to bring the Society into disrepute. 
+					Such expulsion shall be subject to confirmation at the next General Meeting of the Society.</p>
+					
+					<h3>4. THE EXECUTIVE COMMITTEE</h3>
+					<p>4.1 The day-to-day management of the Society's affairs shall be in the hands of the Executive Committee, 
+					which shall normally be elected at the Annual General Meeting (AGM).</p>
+					<p>4.2 The Executive Committee shall consist of a President, Vice-President, Secretary, and Junior 
+					Treasurer, who shall be elected by the members as in 4(1). In addition there shall a Senior Treasurer, 
+					who shall be a resident member of the Senate or other person approved by the Junior Proctor. The Senior 
+					Treasurer shall be appointed by the elected members of the Executive Committee and shall be ex officio 
+					a member of the Executive Committee.</p>
+					<p>4.3 A majority of the elected members of the Executive Committee shall be fulltime undergraduate 
+					or postgraduate students of the University.</p>
+					<p>4.4 Meetings of the Executive Committee shall be chaired by the President or in his or her absence 
+					the Vice-President. If neither the President or Vice-President is present, the remaining members shall 
+					elect a chairperson for that meeting. The quorum for a meeting of the Executive Committee shall be three
+					members and a written record of each meeting shall be kept.</p>
+					
+					<h3>5. GENERAL MEETINGS</h3>
+					<p>5.1 The Society shall hold an Annual General Meeting (AGM) during each academical year. The AGM 
+					shall be held in Cambridge during Easter Full Term. All Members, other than Honorary Members, shall 
+					be entitled to attend and vote at any General Meeting. At least fourteen days written notice shall 
+					be given to members before the AGM.</p>
+					<p>5.2 The AGM shall approve Minutes of the last General Meeting and the Society's Accounts for the 
+					preceding year, elect the Executive Committee for the year ahead and conduct such other business as 
+					is necessary.</p>
+					<p>5.3 Candidates for election to office shall be proposed and seconded by at least one other member. 
+					Every motion at a General Meeting shall be proposed and seconded by two members. Voting shall be by 
+					secret ballot and if there are more than two candidates for a post or more than two options on a motion, 
+					voting shall be by Single Transferable Vote.</p>
+					<p>5.4 An Extraordinary General Meeting (EGM) may be held at any time during Full Term. It may be 
+					called by the Executive Committee or at the written request of at least ten members. Two weeks written 
+					notice shall be given to members before an EGM is held. An EGM shall have the same powers as an AGM.</p>
+					<p>5.5 The President or in his or her absence the Vice-President shall take the Chair at any General 
+					Meeting. In the absence of the President and Vice-President the meeting shall elect a Chairperson for 
+					that meeting. The quorum for a General Meeting shall be five members and a written record of every 
+					General Meeting shall be kept.</p>
+					<p>5.6 All General Meetings should be conducted in Lithuanian language, if demanded translated to 
+					English-speaking members.</p>
+					
+					<h3>6. FINANCIAL MATTERS</h3>
+					<p>6.1 The Society shall maintain a banking account with a suitable Bank or Building Society to hold 
+					the Society's funds.</p>
+					<p>6.2 It shall be the responsibility of the Junior Treasurer to ensure that monies received are 
+					properly accounted for and that the Society's financial records are kept in good order.</p>
+					<p>6.3 The Senior Treasurer shall make arrangements for the Society's Accounts to be properly audited, 
+					either by himself or herself, or by some other person approved under University Ordinances.</p>
+					<p>6.4 The Senior Treasurer shall not be liable for any financial debt or other obligation of the 
+					Society unless he or she has personally authorised such a debt in writing.</p>
+					<p>6.5 For so long as the Society shall be Registered with the Junior Proctor, it shall be the duty 
+					of the Executive Committee to ensure that the Society complies with the requirements for Registration 
+					as a University Society.</p>
+					
+					<h3>7. CHANGES TO THE CONSTITUTION</h3>
+					<p>The Constitution may be amended at a General Meeting.</p>
+					
+					<h3>8. DISOLUTION</h3>
+					<p>8.1 The Society may be dissolved at a General Meeting provided that at least Twenty-one days written 
+					notice of the intention to dissolve the Society has been given to the members. At least two thirds of 
+					those present and voting at the General Meeting must vote in favour of the motion for Dissolution for 
+					it to be effective.</p>
+					<p>8.2 Any motion for Dissolution of the Society shall provide that assets remaining after all 
+					liabilities have been met shall be transferred either to another Registered University Society, 
+					or to the Society's Syndicate.</p>					
+				</div><!-- #post-## -->
+			</div><!-- #content -->
+		</div><!-- #container -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
